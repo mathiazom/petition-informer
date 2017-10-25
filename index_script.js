5fc0060de60945f00aa8002ba39806e87ca07397
Mathiazom <mathiasomyklebust@gmail.com>
2017-10-25 17:34:05 +0200

    Update 25.10.17 17-34
    Update 25.10.17 17-34

--- index_script.js
+++ index_script.js
@@ -4,18 +4,56 @@ window.onscroll = function(){
 
 function isScrolledIntoView(elem)
 {
+    var header = document.getElementById('header');
+    var header_min = document.getElementById('header_min');
+    var go_to_page_top = document.getElementById('go_to_page_top');
+
     if(window.pageYOffset > elem.offsetTop){
-      document.getElementsByTagName('header')[0].style.position = "fixed";
-      document.getElementById('header_subtitle').style.display = "none";
-      document.getElementById('header_title').style.marginTop = "0";
+      header.style.display="none";
+      header_min.style.display="inline-block";
+
+      var go_to_page_top = document.getElementById('go_to_page_top');
+
+      go_to_page_top.style.display = "block";
+
+      var footer = document.getElementsByTagName('footer')[0];
+
+      var pageUpRect = go_to_page_top.getBoundingClientRect();
+      var footerRect = footer.getBoundingClientRect();
+
+      if(window.innerHeight > (footerRect.top + window.innerHeight - pageUpRect.bottom)){
+        go_to_page_top.style.background = "#eee";
+        go_to_page_top.src = "ico/ic_expand_less_blue_24px.svg"
+      }else{
+        go_to_page_top.style.background = "#0277bd";
+        go_to_page_top.src = "ico/ic_expand_less_white_24px.svg"
+      }
+      go_to_page_top.onclick = function(){
+        // ANIMATION
+        var a = window.pageYOffset;
+        setInterval(function(){
+          if(a > 0){
+            if(a > 100){
+              a-=(a/100+100);
+            }else{
+              a-=100;
+            }
+
+            window.scrollTo(0,a);
+          }else{
+          }
+        },0);
+      };
+
     }else{
-      document.getElementsByTagName('header')[0].style.position = "static";
-      document.getElementById('header_subtitle').style.display = "inline-block";
-      document.getElementById('header_title').style.marginTop = "0.67em";
+      header.style.display="inline-block";
+      header_min.style.display="none";
+      go_to_page_top.style.display = "none";
     }
 }
 
 function loadAllPetitions(){
+
   var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
@@ -33,7 +71,7 @@ function AllPetitionsDataReceived(responseText){
     var item = list[i];
     if(item.type == "petition"){
       var el = document.createElement("p");
-      el.innerHTML = "<b>" + item.attributes.action + "</b><br/> <p style='font-size:14px;margin-bottom:0;'><i> by " + item.attributes.creator_name + "</i></p>";
+      el.innerHTML = "<b>" + item.attributes.action + "</b><br/> <p class=all_petitions_list_item_p style='margin-bottom:0;'><i> by " + item.attributes.creator_name + "</i></p>";
       el.className = "all_petitions_list_item";
         el.onclick = (function(i){
           return function(){
